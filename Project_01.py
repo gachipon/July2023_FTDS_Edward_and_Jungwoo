29386b6e8dd149a0b93a1b84a17f7cb1eab2548a
gachipon <gachipon93@gmail.com>
2023-08-17 14:33:59 +0800

    update

--- Project_01.py
+++ Project_01.py
@@ -1,4 +1,4 @@
-import os.path
+import datetime
 
 def calculate_annual_deposits():
     if interest_rate_value != 0:
@@ -18,56 +18,63 @@ def tabulate_annual_deposits():
     return end_of_year_list
 
 def create_table():
-    with open("Test.txt", "w") as my_file:
+    time_now = datetime.datetime.now().strftime("%c")
+    with open(f"Document_on_{time_now}.txt", "w") as my_file:
+        my_file.write(f"Created on {time_now}\n\n")
         for i in range(len(end_of_year_values)):
             my_file.write(f"Year {i + 1}\t|\t$ {end_of_year_values[i]} \n")
 
-# The different variables
-current_age = int(input("How old are you right now?: "))
-retiring_age = int(input("At what age do you wish to retire?: "))
-if retiring_age < current_age:
-    print("You shouldn't be using this if you are already retired.")
-    exit()
-interest_rate = float(input("What is your expected annual return rate?: "))
-if interest_rate == 0:
-    print("Perpetuity cannot have 0% interest..., You might as well store money under your bed.")
-    exit()
-annual_dividends = float(input("How much will you spend per year after you retire?: "))
-
-number_of_contributions = retiring_age - current_age
-interest_rate_value = interest_rate/100
-
-savings_required = annual_dividends/interest_rate_value
-
-
-annual_deposits = calculate_annual_deposits()
-end_of_year_values = tabulate_annual_deposits()
-
-attempts = 0
 while True:
-    options = input("\nWhat would you like to see?: "
-                    "\n1.\tAnnual deposits. "
-                    "\n2.\tTotal Expected Savings. "
-                    "\n3.\tBalance at end of each year. "
-                    "\n4.\tTabulate in a document. "
-                    "\n5.\tClose program. "
-                    "\nSelect your option:\t")
-    if options == '1':
-        print(f"You will need to deposit ${annual_deposits} every year for {number_of_contributions} years.")
-    elif options == '2':
-        print(f"Your total savings at the end of {number_of_contributions} years will be ${savings_required}. This value is rounded down.")
-    elif options == '3':
-        for i in end_of_year_values:
-            print("Year ", end_of_year_values.index(i)+1, "\t:\t", i)
-    elif options == '4':
-        create_table()
-        print("Successfully printed Table in a new document.")
-    elif options == '5':
-        print("Shutting Down...")
-        break
-    else:
-        if attempts >= 3:
+# The different variables
+    current_age = int(input("How old are you right now?: "))
+    retiring_age = int(input("At what age do you wish to retire?: "))
+    if retiring_age < current_age:
+        print("You shouldn't be using this if you are already retired.")
+        exit()
+    interest_rate = float(input("What is your expected annual return rate?: "))
+    if interest_rate == 0:
+        print("Perpetuity cannot have 0% interest..., You might as well store money under your bed.")
+        exit()
+    annual_dividends = float(input("How much will you spend per year after you retire?: "))
+
+    number_of_contributions = retiring_age - current_age
+    interest_rate_value = interest_rate/100
+
+    savings_required = annual_dividends/interest_rate_value
+
+    annual_deposits = calculate_annual_deposits()
+    end_of_year_values = tabulate_annual_deposits()
+
+    attempts = 0
+    while True:
+        options = input("\nWhat would you like to see?: "
+                        "\n1.\tAnnual deposits. "
+                        "\n2.\tTotal Expected Savings. "
+                        "\n3.\tBalance at end of each year. "
+                        "\n4.\tTabulate in a document. "
+                        "\n5.\tClose program. "
+                        "\n6.\tReset Variables. "
+                        "\nSelect your option:\t")
+        if options == '1':
+            print(f"You will need to deposit ${annual_deposits} every year for {number_of_contributions} years.")
+        elif options == '2':
+            print(f"Your total savings at the end of {number_of_contributions} years will be ${savings_required}. This value is rounded down.")
+        elif options == '3':
+            for i in end_of_year_values:
+                print("Year ", end_of_year_values.index(i)+1, "\t:\t", i)
+        elif options == '4':
+            create_table()
+            print("Successfully printed Table in a new document. Now shutting down...")
+            exit()
+        elif options == '5':
+            print("Shutting Down...")
+            exit()
+        elif options == '6':
+            print("Reseting to new variables...")
             break
         else:
-            print(f"That input is not an available option. You have {3 - attempts} more attempts before we shut down.")
-            attempts += 1
+            if attempts >= 3:
+                exit()
+            else:
+                print(f"That input is not an available option. You have {3 - attempts} more attempts before we shut down.")
+                attempts += 1
