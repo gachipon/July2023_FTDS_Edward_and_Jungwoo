77bb36848f40e9c320b5383dd3188de864f7c19f
gachipon <gachipon93@gmail.com>
2023-08-17 18:32:58 +0800

    a
    a

--- Project_01.py
+++ Project_01.py
@@ -1,22 +1,28 @@
+# Import datetime is for importing date/time module for python to easily get time stamps
 import datetime
 
+# This function just calculates how much should be deposit in the account each year
 def calculate_annual_deposits():
+    # Interest rate cannot be zero...because you cannot have a perpetuity account with 0 % interest
     if interest_rate_value != 0:
         annual_deposits = savings_required / (( 1 + interest_rate_value ) * ((( 1 + interest_rate_value) ** number_of_contributions ) - 1 ) / interest_rate_value)
     else:
         annual_deposits = savings_required/number_of_contributions
-    return round(annual_deposits, 2)
+    return round(annual_deposits, 2) #The '2' represents number of decimal points the result is returned
+    # Money is usually counted to cents...this may result in some rounding...but its pretty negligible
 
 # Loop based function to show the value accumulated every year
 # Nice to show each value in a table or list
 def tabulate_annual_deposits():
     end_of_year_value = 0
     end_of_year_list = []
+    # Loop will add the end of year contribution (along with the compounded interest) for each year
     for i in range(number_of_contributions):
         end_of_year_value = (end_of_year_value + calculate_annual_deposits()) * ( 1 + interest_rate_value)
         end_of_year_list.append(round(end_of_year_value, 2))
     return end_of_year_list
 
+# This creates a more visual look at the amount at end of each year
 def create_table():
     time_now = datetime.datetime.now().strftime("%c")
     with open(f"Document_on_{time_now}.txt", "w") as my_file:
